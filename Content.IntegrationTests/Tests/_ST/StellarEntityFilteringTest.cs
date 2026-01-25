2026a43a277a32c6016187def8267776431f7706
Ed <edwardxperia2000@gmail.com>
2026-01-25 13:15:04 +0300

    ForkFiltered

---
+++ Content.IntegrationTests/Tests/_ST/StellarEntityFilteringTest.cs
@@ -0,0 +1,40 @@
+#nullable enable
+using System.Collections.Generic;
+using Robust.Shared.Prototypes;
+
+namespace Content.IntegrationTests.Tests._ST;
+
+[TestFixture]
+public sealed class StellarEntityFilteringTest
+{
+    [Test]
+    public async Task CheckAllEntityHasForkFilteredCategory()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
+
+        var protoManager = server.ResolveDependency<IPrototypeManager>();
+
+        await server.WaitAssertion(() =>
+        {
+            Assert.Multiple(() =>
+            {
+                if (!protoManager.TryIndex<EntityCategoryPrototype>("ForkFiltered", out var indexedFilter))
+                    return;
+
+                foreach (var proto in protoManager.EnumeratePrototypes<EntityPrototype>())
+                {
+                    if (!proto.ID.Contains("Stellar"))
+                        continue;
+
+                    if (proto.Abstract || proto.HideSpawnMenu)
+                        continue;
+
+                    if (!proto.Categories.Contains(indexedFilter))
+                        Assert.Fail($"EntityPrototype: {proto} is not marked abstract, or does not have a HideSpawnMenu or ForkFiltered category");
+                }
+            });
+        });
+        await pair.CleanReturnAsync();
+    }
+}
---
+++ Content.Stellar.Client/Entry/ForkFilteredSystem.cs
@@ -0,0 +1,18 @@
+using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Stellar.Client.Entry;
+
+/// <summary>
+/// On the client side, it automatically enables entity filtering to hide all vanilla ss14 entities
+/// not marked with the ForkFiltered category from the spawn menu.
+/// </summary>
+public sealed class ForkFilteredSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    public override void Initialize()
+    {
+        _cfg.SetCVar(CVars.EntitiesCategoryFilter, "ForkFiltered");
+    }
+}
